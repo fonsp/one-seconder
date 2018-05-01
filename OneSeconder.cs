f82acb1390ca8028a6c8476a2ce4e09272eb6004
Fons van der Plas <fonsvdplas@gmail.com>
2018-05-01 15:53:23 +0200

    Variable length per day ⏱

--- OneSeconder.cs
+++ OneSeconder.cs
@@ -1,6 +1,6 @@
 /*
 one-seconder
-Copyright 2017, Fons van der Plas
+Copyright 2018, Fons van der Plas
 http://github.com/fons-/one-seconder
 
 AFTER SELECTING SECONDS:
@@ -16,28 +16,34 @@ file './2017-09-01.mkv'
 etc
 
 BEFORE SELECTING SECONDS:
+Make sure that every video is 1080p, horizontal, and 30fps.
+Use the following commands, or the *.sh scripts in this repository (which also preserve the timestamp).
 
 Rotate video with:
 $ ffmpeg -i in.mov -vf 'transpose=2' -strict -2 out.mov
 
-transpose:
+transpose
 1 = 90Clockwise
 2 = 90CounterClockwise
 
-Flip video with:
+Flip a video 180 degrees with:
 $ ffmpeg -i in.mov -vf 'hflip,vflip' -strict -2 out.mov
 
 Pad a vertical video with:
-$ ffmpeg -i IMG_0131.MOV -filter_complex 'scale=607:1080, pad=1920:1080:656:0:black' -strict -2 IMG_0131_pad.mov
+$ ffmpeg -i in.mov -filter_complex 'scale=607:1080, pad=1920:1080:656:0:black' -strict -2 out.mp4
+
+Upscale a (horizontal) video to 1080p:
+$ ffmpeg -i in.mp4 -vf scale=1920:1080 -strict -2 out.mp4
+
+Convert a 120fps iPhone slo-mo video to a slow 30fps video (remember to upscale the output to 1080p):
+$ ffmpeg -i in.mov -vf "setpts=4.0*PTS" -af "atempo=0.5,atempo=0.5" -r 30 -strict -2 out.mp4
 
 MISCELLANEOUS:
-
-On Windows 10 with iOS: import videos using the Photos app to preserve timestamps. 
+On Windows 10 with iOS: import videos using the Photos app to preserve timestamps.
 On Windows: Use BulkFileChanger to change timestamps.
 
 Copy 'modified' date (used by the program) between files:
 $ touch -r from.mp4 to.mp4
-
 
 */
 
@@ -65,7 +71,7 @@ public class Program
 		proc.WaitForExit();
 
 		string output = proc.StandardOutput.ReadToEnd();
-		
+
 		string[] outputLines = output.Split(new char[] { '\r', '\n', '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 		if (outputLines.Length != 3)
 		{
@@ -79,9 +85,9 @@ public class Program
 		}
 	}
 
-	static private void CreateTestVideo(string filename, double offset)
+	static private void CreateTestVideo(string filename, double offset, double length)
 	{
-		ProcessStartInfo psi = new ProcessStartInfo("ffmpeg", "-y -v error -i \"" + filename + "\" -ss " + offset + " -t 1 -codec copy testScreen.mp4");
+		ProcessStartInfo psi = new ProcessStartInfo("ffmpeg", "-y -v error -i \"" + filename + "\" -ss " + offset + " -t " + length + " -codec copy testScreen.mp4");
 		psi.UseShellExecute = false;
 		psi.CreateNoWindow = true;
 		psi.RedirectStandardOutput = false;
@@ -107,12 +113,12 @@ public class Program
 		proc.WaitForExit();
 	}
 
-	static private void CreateVideo(string filename, double offset, DateTime dt, bool writeTxt)
+	static private void CreateVideo(string filename, double offset, double length, DateTime dt, bool writeTxt)
 	{
 		if(writeTxt){
-			File.WriteAllLines(CreateFilename(dt) + ".txt", new string[] {filename, offset.ToString()});
+			File.WriteAllLines(CreateFilename(dt) + ".txt", new string[] {filename, offset.ToString(), length.ToString()});
 		}
-		ProcessStartInfo psi = new ProcessStartInfo("ffmpeg", "-y -v error -i \"" + filename + "\" -ss " + offset + " -t 1 -vf drawtext=\"fontfile =/usr/share/fonts/truetype/lato/Lato-Bold.ttf: text = '" + CreateDateString(dt) + "': fontcolor = white: fontsize = 48: box = 1: boxcolor = black@0.5: boxborderw = 10: x = w / 32: y = h - w / 32 - text_h\" -codec:a copy -codec:v libx264 -preset slow -strict -2 " + CreateFilename(dt) + ".mkv");
+		ProcessStartInfo psi = new ProcessStartInfo("ffmpeg", "-y -v error -i \"" + filename + "\" -ss " + offset + " -t " + length + " -vf drawtext=\"fontfile =/usr/share/fonts/truetype/lato/Lato-Bold.ttf: text = '" + CreateDateString(dt) + "': fontcolor = white: fontsize = 48: box = 1: boxcolor = black@0.5: boxborderw = 10: x = w / 32: y = h - w / 32 - text_h\" -codec:a copy -codec:v libx264 -preset slow -strict -2 " + CreateFilename(dt) + ".mkv");
 		psi.UseShellExecute = false;
 		psi.CreateNoWindow = true;
 		psi.RedirectStandardOutput = true;
@@ -140,7 +146,6 @@ public class Program
 	static public void Main()
 	{
 		string folderName = null;
-		//folderName = "/mnt/c/Users/Fons/OneDrive/onesecondaday(5jul17-present)/";
 		while (string.IsNullOrWhiteSpace(folderName) || !Directory.Exists(folderName))
 		{
 			Console.WriteLine("Folder name:");
@@ -150,7 +155,7 @@ public class Program
 		List<string> fileNames = Directory.GetFiles(folderName).ToList();
 		int count = fileNames.Count;
 
-		Console.WriteLine("Processing files...");
+		Console.WriteLine("Reading video lengths...");
 
 		List<double> lengths = new List<double>();
 		List<int> widths = new List<int>();
@@ -173,9 +178,15 @@ public class Program
 		Console.CursorLeft = 0;
 		Console.WriteLine("100%");
 
+		double lengthPerDay = -1.0;
+		while(lengthPerDay<=0.0){
+			Console.WriteLine("How many seconds per day? (Choose 1.0 for the classic length.)");
+			double.TryParse(Console.ReadLine(), out lengthPerDay);
+		}
+
 		//var toRemove = Enumerable.Range(0, count).Where(i => lengths[i] < 1.0 || widths[i] != 1920 || heights[i] != 1080).ToList();
-		var toRemove = Enumerable.Range(0, count).Where(i => lengths[i] < 1.0).ToList();
-		Console.WriteLine("Ignoring {0} short (<1 sec) clips.", toRemove.Count);
+		var toRemove = Enumerable.Range(0, count).Where(i => lengths[i] < lengthPerDay).ToList();
+		Console.WriteLine("Ignoring {0} short (<"+lengthPerDay+" sec) clips.", toRemove.Count);
 
 		for(int i = 0; i < toRemove.Count; i++)
 		{
@@ -188,10 +199,10 @@ public class Program
 
 		var firstDay = dates.Min().Date;
 		var lastDay = dates.Max().Date;
-		
+
 		for(DateTime date = firstDay; date <= lastDay; date = date.AddDays(1))
 		{
-			
+
 			List<int> indices = Enumerable.Range(0, count).Where(i => dates[i].Date == date).ToList();
 			Console.WriteLine("{0}: {1} videos found.", date.ToShortDateString(), indices.Count);
 			if(File.Exists(CreateFilename(date) + ".txt")){
@@ -215,7 +226,7 @@ public class Program
 							Console.WriteLine("Choose video: [1 - {0}]", num);
 							int.TryParse(Console.ReadLine(), out index);
 						}
-						index--;	
+						index--;
 					}
 					int vidNum = indices[index];
 					double length = lengths[vidNum];
@@ -224,7 +235,7 @@ public class Program
 					double offset = -1.0;
 					while ((!skip) && (offset < 0.0 || offset >= length))
 					{
-						Console.WriteLine("Choose offset: [0.0 - {0}] or [full] or [skip]", length - 1.0);
+						Console.WriteLine("Choose offset: [0.0 - {0}] or [full] or [skip]", length - lengthPerDay);
 						string input = Console.ReadLine();
 						if (input == "full")
 						{
@@ -244,18 +255,18 @@ public class Program
 
 					if (!skip)
 					{
-						CreateTestVideo(fileNames[vidNum], offset);
+						CreateTestVideo(fileNames[vidNum], offset, lengthPerDay);
 
 						Console.WriteLine("Satisfied? [y/n] or [r] to render without continuing");
 						string consoleInput = Console.ReadKey().KeyChar.ToString();
+						Console.WriteLine();
 						if(consoleInput == "r"){
-							CreateVideo(fileNames[vidNum], offset, dates[vidNum], false);
+							CreateVideo(fileNames[vidNum], offset, lengthPerDay, dates[vidNum], false);
 						}
 						done = consoleInput.ToLower() == "y";
-						Console.WriteLine();
 						if (done)
 						{
-							CreateVideo(fileNames[vidNum], offset, dates[vidNum], true);
+							CreateVideo(fileNames[vidNum], offset, lengthPerDay, dates[vidNum], true);
 						}
 					}
 				}
