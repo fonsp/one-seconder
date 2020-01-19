a36497fb49a4b193bb07312373cd19ce8434e4d6
Fons van der Plas <fonsvdplas@gmail.com>
2020-01-19 16:49:11 +0100

    Compression for Osmo Action videos

---
+++ compress_osmo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# mkdir originals &> /dev/null
+
+for FILE1 in "$@"
+do
+ffmpeg -i "$FILE1" -c:a copy -c:v libx264 -crf 22 -tune grain -y -map_metadata 0 "compressed_$FILE1"
+# touch -r "$FILE1" "rot$FILE1"
+# mv "$FILE1" "originals/$FILE1"
+done
