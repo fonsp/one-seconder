91db8ff333df46653392cb334c52ebfd5190d64f
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-06 14:07:37 +0100

    Update compress_osmo.sh

--- compress_osmo.sh
+++ compress_osmo.sh
@@ -5,6 +5,6 @@
 for FILE1 in "$@"
 do
 ffmpeg -i "$FILE1" -c:a copy -c:v libx264 -crf 22 -tune grain -y -map_metadata 0 "compressed_$FILE1"
-# touch -r "$FILE1" "rot$FILE1"
+touch -r "$FILE1" "compressed_$FILE1"
 # mv "$FILE1" "originals/$FILE1"
 done
